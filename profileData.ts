f6106d3cf0040310457377c6a25123eda06da1c2
Benjamin W. <benjaminsw@users.noreply.github.com>
2025-12-07 08:39:40 +0000

    Fix formatting of keywords in research focus

--- profileData.ts
+++ profileData.ts
@@ -35,7 +35,7 @@ export const profileData: ProfileData = {
     "area": "Variational Inference",
     "keywords": [
       "Bayesian Inference",
-      "Uncertainty Quantification"
+      "Uncertainty Quantification",
       "Variational Inference",
       "Mixture Model",
       "Normalising Flows",
